0311ed8c4246fa75a5b3de7a1d7b3905949b5de5
esakenyun <rfahlevih@gmail.com>
2026-07-04 22:08:21 +0700

    refactor: remove deprecated admin dashboard pages and management modules

--- app/dashboard/admin/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ContentLayout } from "@/components/dashboard-panel/content-layout";
-
-export default function DashboardAdmin() {
-  return (
-    <ContentLayout title="Dashboard Admin">
-      <h1>Dashboard Admin</h1>
-    </ContentLayout>
-  );
-}
